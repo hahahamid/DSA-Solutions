6b1e6fc638e267f6ce5ddfc1ac70206ff72021a6
Miran Hamid <44381554+hahahamid@users.noreply.github.com>
2023-07-11 20:58:52 +0530

    Create 994. Rotting Oranges.cpp

---
+++ 994. Rotting Oranges.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size(); 
+        int m = grid[0].size(); 
+
+        vector<vector<int>>vis(n, vector<int>(m, 0)); 
+
+        queue<pair<pair<int,int>, int>> Q; 
+
+        int cntFresh(0); 
+
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(grid[i][j] == 2){
+                    Q.push({{i, j}, 0}); 
+                    vis[i][j] = 2; 
+                }
+                if(grid[i][j] == 1) cntFresh++; 
+            }
+        }
+
+
+        int dr[] = {-1, 0, 1, 0}; 
+        int dc[] = {0, 1, 0, -1}; 
+
+        int cnt(0);
+        int ans(0); 
+
+        while(!Q.empty()){
+            int r = Q.front().first.first; 
+            int c = Q.front().first.second; 
+            int t = Q.front().second; 
+            Q.pop(); 
+
+            ans = max(ans, t); 
+
+
+            for(int i = 0; i<4; i++){
+                int nr = r + dr[i];  
+                int nc = c + dc[i]; 
+
+                if(nr >= 0 and nr < n and nc >=0 and nc<m and grid[nr][nc] == 1 and vis[nr][nc] != 2){
+                    Q.push({{nr, nc}, t+1}); 
+                    vis[nr][nc] = 2; 
+                    cnt++; 
+                }
+            }
+        }
+
+        if(cnt != cntFresh) return -1; 
+        return ans;  
+
+    }
+};
+
